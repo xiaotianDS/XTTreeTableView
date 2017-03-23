0682fafd6f646b2da26e5b4fa41a032ba01f0a4e
xiaotianDS <xiaotianDS@163.com>
2017-03-23 12:01:46 +0800

    Model Annotate
    Add annotate for ListModel

--- iOS中n层树状结构解析/ListModel.h
+++ iOS中n层树状结构解析/ListModel.h
@@ -13,11 +13,11 @@
 @property(nonatomic,copy)NSString *name;
 //@property(nonatomic,copy)NSString *nameId;
 @property(nonatomic,copy)NSNumber *nameId;
-@property(nonatomic,copy)NSArray<ListModel *> *subList;
-@property(nonatomic,assign)BOOL isExpand;
+@property(nonatomic,copy)NSArray<ListModel *> *subList;//子类Model的数组
+@property(nonatomic,assign)BOOL isExpand;//是否展开的状态
 
-@property(nonatomic,weak)ListModel *superModel;
-@property(nonatomic,assign)int level;
+@property(nonatomic,weak)ListModel *superModel;//父类Model
+@property(nonatomic,assign)int level;//当前Model所在的层级（第几级的Model）
 
 - (instancetype)initWithDict:(NSDictionary *)dict;
 + (instancetype)listModelWithDict:(NSDictionary *)dict;
